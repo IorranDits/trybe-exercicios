0fcb025a3d2864b9e676bdb9108ad024cfe0afaa
Iorran Ditscheiner <iorrandits@gmail.com>
2022-06-22 09:45:28 -0300

    Adicionando pastas de dias de exercicios

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-05/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-05/scripts.js
@@ -1,14 +1,15 @@
+
+//  1- operadores aritméticos
 const a = 4;
 const b = 7;
 
-// operadores aritméticos
 console.log(a + b);
 console.log(a - b);
 console.log(a * b);
 console.log(a / b);
 console.log(a % b);
 
-//Retornar maior de dois números
+// 2 - Retornar maior de dois números
 const num1 = 5;
 const num2 = 10;
 
@@ -19,7 +20,7 @@ if (num1 > num2) {
   console.log(num2);
 }
 
-//Retornar maior de 3 números
+// 3 -Retornar maior de 3 números
 const n1 = 30;
 const n2 = 25;
 const n3 = 10;
@@ -34,7 +35,7 @@ if (n1 > n2 && n1 > n3) {
   console.log(n3);
 }
 
-// Retornnar se é positivo ou negativo
+// 4 - Retornnar se é positivo ou negativo
 const number = -5;
 
 if (number < 0) {
@@ -47,7 +48,7 @@ if (number < 0) {
   console.log('positive');
 }
 
-//ângulos de um triângulo
+// 5 - ângulos de um triângulo
 const angulo1 = 90;
 const angulo2 = 45;
 const angulo3 = 45;
@@ -64,6 +65,8 @@ if (angulo1 > 0 && angulo2 > 0 && angulo3 > 0) {
 }else {
   console.log('Error');
 }
+
+
 
 
 

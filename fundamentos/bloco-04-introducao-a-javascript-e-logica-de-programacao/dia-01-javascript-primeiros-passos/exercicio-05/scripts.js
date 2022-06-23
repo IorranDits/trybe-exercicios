264c8aa9d8897da8a40aea58f889f08f4702d307
Iorran Ditscheiner <iorrandits@gmail.com>
2022-06-23 10:23:41 -0300

    Exercícios da parte da tarde efetuados

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-05/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-05/scripts.js
@@ -66,7 +66,7 @@ if (angulo1 > 0 && angulo2 > 0 && angulo3 > 0) {
   console.log('Error');
 }
 
-//Peça de xadrez
+// 6 - Peça de xadrez
 let piece = 'peão';
 
 switch (piece.toLocaleLowerCase()) {
@@ -92,7 +92,113 @@ switch (piece.toLocaleLowerCase()) {
     console.log('Erro: peça inválida.')
 }
 
+// 7 - Notas convertidas
+let nota = 100;
 
+if (nota < 0 || nota > 100) {
+  console.log('Erro, nota inválida');
+
+}else if (nota < 50) {
+  console.log('Conceito F');
+
+}else if (nota < 60) {
+  console.log('Conceito E');
+
+}else if (nota < 70) {
+  console.log('Conceito D');
+
+}else if (nota < 80) {
+  console.log('Conceito C');
+
+}else if (nota < 90) {
+  console.log('Conteito B');
+
+}else {
+  console.log('Conceito A');
+
+}
+
+// 8 -Verificar se ao menos um número é par
+const number1 = 2;
+const number2 = 3;
+const number3 = 7;
+
+if (number1 % 2 === 0 || number2 % 2 === 0 || number3 % 2 === 0) {
+  console.log(true);
+
+}else {
+  console.log(false);
+
+}
+
+
+// 9 - Verificar se ao menor um número é impar 
+const numb1 = 300;
+const numb2 = 3;
+const numb3 = 8;
+
+if (numb1 % 2 === 0 && numb2 % 2 === 0 && numb3 % 2 === 0) {
+  console.log(false);
+
+}else {
+  console.log(true);
+
+}
+
+// 10 - Custo e valor de venda
+const custo = 300;
+const venda = 150;
+
+const custoTotal = custo + (custo * 0.2);
+const lucro = venda - custoTotal;
+
+if (custo < 0 || venda < 0) {
+  console.log("Erro: valor inválido");
+  
+}else {
+  console.log(lucro);
+}
+
+// 11 - Descontos do salário
+const salario = 3000; // Salário Bruto
+let inss; // Alícota do INSS
+
+if (salario <= 1556.94) {
+  inss = (salario * 0.08);
+
+}else if (salario <= 2594.92) {
+  inss = (salario * 0.09);
+
+}else if (salario <= 5189.82) {
+  inss = (salario * 0.11);
+
+}else {
+  inss = 570.88;
+
+}
+
+let salarioBase = salario - inss;
+let ir; //Imposto d Renda
+let salarioLiquido;
+
+if (salarioBase <= 1903.98) {
+  ir = 0;
+
+}else if (salarioBase <= 2826.65) {
+  salarioLiquido = (salarioBase - (salarioBase * 0.075)) + 142.80; 
+
+}else if (salarioBase <= 3751.05) {
+  salarioLiquido = (salarioBase - (salarioBase * 0.15)) + 354.80; 
+
+}else if (salarioBase <= 4664.68) {
+  salarioLiquido = (salarioBase - (salarioBase * 0.225)) + 636.13;
+
+}else {
+  salarioLiquido = (salarioBase - (salarioBase * 0.275)) + 869.36;
+
+}
+
+console.log('Salário líquido: R$ ' + salarioLiquido);
 
 
 
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicio-1-arrays/arrays.js
@@ -0,0 +1,24 @@
+//Exercício 1
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+//Exercício 2 
+
+let menu1 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu1[2] ;
+
+console.log(indexOfPortfolio);
+
+
+// Exercício 3
+
+let menu3 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu3.push('Contato');
+
+console.log(menu3);
+
+console.log(menu3.length);

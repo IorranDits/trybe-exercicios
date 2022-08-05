43eb0e2a8d829eff38c8606b0d516b3e540d273f
Iorran Ditscheiner <iorrandits@gmail.com>
2022-08-05 15:03:04 -0300

    Efetuando exerciicos 1 a 4 do bloco 9.1

--- desenvolvimento-front-end/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercicios/exercicio1.js
+++ desenvolvimento-front-end/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercicios/exercicio1.js
@@ -25,6 +25,35 @@ const jupiter = {
   },
 };
 
-console.log(planetDistanceFromSun(mars)); // A
-console.log(planetDistanceFromSun(venus)); // B
-console.log(planetDistanceFromSun(jupiter)); // C
+//Exercíco 03:
+
+const getPlanet = () => {
+  const mars = {
+    name: 'Mars',
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: 'kilometers',
+    },
+  };
+  console.log('Returned planet: ', mars);
+};
+
+
+//Exercçio 04: 
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = () => {
+  console.log(`A temperatura de Marte é: ${getMarsTemperature()} graus celsius`);
+}
+
+// Imprime "A temperatura de Marte é: 20 graus celsius", por exemplo
+// setTimeout(() => sendMarsTemperature(), messageDelay());
+
+//Exercicio 05: 
